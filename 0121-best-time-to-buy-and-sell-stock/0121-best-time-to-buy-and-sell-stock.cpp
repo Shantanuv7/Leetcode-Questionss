3c540a2daa5f178e4445ab07c575b28b00d4e9b2
Shantanuv7 <genaishantanu@gmail.com>
2026-08-13 03:29:09 +0530

    Time: 3 ms (27.74%), Space: 97.3 MB (61.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,15 +5,12 @@ public:
         int bestbuy = nums[0];
         int maxy = 0;
 
-        for (int i = 1 ; i< nums.size(); i++)
-        {    
-           if (nums[i] > bestbuy)
-           {
-            maxy = max(maxy, nums[i]- bestbuy);
-           }
-
-            bestbuy = min(bestbuy,nums[i]);
+        for (int i = 1; i < nums.size(); i++)
+        {
+            maxy = max(maxy, nums[i] - bestbuy);
+            bestbuy = min(bestbuy, nums[i]);
         }
+
         return maxy;
     }
 };
