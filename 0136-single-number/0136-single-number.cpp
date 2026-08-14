459b4913288356d46bf2eb6c0d18b7af44087d64
Shantanuv7 <genaishantanu@gmail.com>
2026-08-15 00:18:10 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.25%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         
+         int ans;
+            ans = 0;
+
+        for (int i=0; i < nums.size(); i++)
+        {
+            
+            ans = nums[i] ^ ans;
+        }
+        return ans;
+    }
+};
