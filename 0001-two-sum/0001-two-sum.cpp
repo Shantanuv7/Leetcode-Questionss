500b611d1c98ba9f6dc2131799b5f52e51e664d3
Shantanuv7 <genaishantanu@gmail.com>
2026-08-24 00:31:30 +0530

    Time: 3 ms (66.72%), Space: 14.9 MB (42.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,7 +8,7 @@ public:
 
       for (int i = 0; i < nums.size(); i++)
       {    
-         int required = target - nums[i];
+         int required = target - nums[i]; 
         if (mpp.find(required) != mpp.end())
         {
             return {mpp[required], i};
